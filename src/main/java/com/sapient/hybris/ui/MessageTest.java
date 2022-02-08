c51deb4204324dc5394074273c2fc033f767f054
Udaykrusha <udaykrushnajena8984@gmail.com>
2022-02-08 10:08:07 -0800

    first commit

---
+++ src/main/java/com/sapient/hybris/ui/MessageTest.java
@@ -0,0 +1,14 @@
+package com.sapient.hybris.ui;
+import com.sapient.hybris.service.MessageBuilder;
+
+public class MessageTest {
+
+	public static void main(String[] args) {
+		MessageBuilder messageBuilder = new MessageBuilder();
+		
+		System.out.println(messageBuilder.getMessage(null));
+		System.out.println(messageBuilder.getMessage("Srini"));
+
+	}
+
+}
